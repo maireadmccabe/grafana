81158672886fbaba235890932a8532e77055e49c
Mairead McCabe <mairead.mccabe@sonalake.com>
2017-09-06 09:10:07 +0100

    Remove "text" option from mode dropdown, Remove associated unused code.

--- public/app/plugins/panel/text/module.ts
+++ public/app/plugins/panel/text/module.ts
@@ -28,6 +28,10 @@ export class TextPanelCtrl extends PanelCtrl {
   onInitEditMode() {
     this.addEditorTab('Options', 'public/app/plugins/panel/text/editor.html');
     this.editorTabIndex = 1;
+
+    if (this.panel.mode === 'text') {
+      this.panel.mode = 'markdown';
+    }
   }
 
   onRefresh() {
@@ -39,19 +43,8 @@ export class TextPanelCtrl extends PanelCtrl {
       this.renderMarkdown(this.panel.content);
     } else if (this.panel.mode === 'html') {
       this.updateContent(this.panel.content);
-    } else {
-      this.renderText(this.panel.content);
     }
     this.renderingCompleted();
-  }
-
-  renderText(content) {
-    content = content
-    .replace(/&/g, '&amp;')
-    .replace(/>/g, '&gt;')
-    .replace(/</g, '&lt;')
-    .replace(/\n/g, '<br/>');
-    this.updateContent(content);
   }
 
   renderMarkdown(content) {
